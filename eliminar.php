0fc6cd901486816fe24c4eca55627c679cf07acf
aalejandrobernal <aalejandrobernal@gmail.com>
2022-09-25 14:49:04 -0500

    mi primer proyecto

---
+++ eliminar.php
@@ -0,0 +1,13 @@
+<?php
+    include("cn.php");
+
+    $id = $_GET['id'];
+
+    $eliminar = "DELETE FROM pelicula1 WHERE ID ='$id'";
+    $resultadoEliminar=mysqli_query($conexion, $eliminar);
+
+    if($resultadoEliminar){
+        echo "<script>alert('REGISTRO ELIMINADO');window.location='/nuevo/modificar.php';</script>";
+    } else{
+        echo "<script>alert('NO SE ELIMINO'); window.history.go(-1);</script>";
+    }
